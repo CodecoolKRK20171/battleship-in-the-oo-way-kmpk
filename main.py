1c6187cdd5f008d6280a07d1ec23a5ed981d8e10
Michał Urban <michalurban23@gmail.com>
2017-06-13 14:04:40 +0200

    cyce hahahaha

--- main.py
+++ main.py
@@ -1,10 +1,62 @@
-# Plan:
-# 
-
-
 from ocean import Ocean
+from player import Player
 
-bajer = Ocean()
-bajer.fill_board()
 
-print(bajer)
+def create_game():
+    # pytaj o ilość graczy
+    # pytaj o imię
+    player1 = Player("Gracz")
+    player2 = Player("AI")
+    # kreacja startowego boardu
+    board1 = Ocean(player1)
+    board2 = Ocean(player2)
+
+    return board1, board2, player1, player2
+
+
+def check_end_game(player1, player2):
+    if not player1.is_alive:
+        show_lose_screen()
+    if not player2.is_alive:
+        show_win_screen()
+
+
+def print_boards(board1, board2):
+    # board1.fill_board()
+    print(board1)
+    # board2.fill_board()
+    print(board2)
+
+
+def ask_for_positions():
+    x = input("X ")
+    y = input("Y ")
+    return int(x), int(y)
+
+
+def main():
+    board1, board2, player1, player2 = create_game()
+    board1.fill_board()
+    board2.fill_board()
+    while True:
+        print_boards(board1, board2)
+        # jakieś komunikaty, pytanie o pozycje
+        x, y = ask_for_positions()
+        shoot = player1.shoot_on_board(board2, x, y)
+        # jakiś komunikat
+        x, y = ask_for_positions()
+        shoot = player2.shoot_on_board(board1, x, y)
+        # jakiś komunikat
+        check_end_game(player1, player2)
+
+
+def show_lose_screen():
+    pass
+
+
+def show_win_screen():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- ocean.py
+++ ocean.py
@@ -3,20 +3,24 @@ from square import Square
 
 class Ocean:
 
-    def __init__(self):
+    def __init__(self, owner):
         self.ships = []
         self.board = []
+        self.owner = owner
 
     def fill_board(self):
-
         for i in range(0, 10):
+            temp_list = []
             for j in range(0, 10):
-                self.board.append(Square(i, j))
+                temp_list.append(Square(i, j))
+            temp_list.append("\n")
+            self.board.append(temp_list)
 
     def __str__(self):
         squares = ''
 
         for square in self.board:
-            squares += str(square)
+            for square2 in square:
+                squares += str(square2)
 
         return squares
