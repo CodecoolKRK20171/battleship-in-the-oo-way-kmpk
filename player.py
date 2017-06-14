9a768be66a9b9006674c1a8727492aa532b803bb
Pablo <pablo.kulpa@gmail.com>
2017-06-14 09:39:31 +0200

    Update player

--- player.py
+++ player.py
@@ -9,10 +9,10 @@ class Player:
         self.is_alive = True
 
     def shoot_on_board(self, board, x, y):
-        if board.board[y][x].is_ship:
-            board.board[y][x].mark()
+        if board.board[y][x+1].is_ship:
+            board.board[y][x+1].mark()
         else:
-            board.board[y][x].show()
+            board.board[y][x+1].show()
 
     def is_alive(self, board):
         available_square_ship = 0
