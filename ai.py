66d84d8455f2bd098e946aa2e607c1e4f67b0bad
Kacper Zelichowski <kafejek@gmail.com>
2017-06-16 09:32:42 +0200

    AI doctrings added.

--- ai.py
+++ ai.py
@@ -4,11 +4,30 @@ from random import randint
 
 class AI:
     def __init__(self):
+        """Initialize function. Createas attributes needed for AI.
+
+        Args:
+            none
+
+        Returns:
+            none
+
+        """
         self.marked = []
         self.unmarked = []
         self.shooted_target = []
 
     def shoot_on_board_ai(self, battlefield):
+        """AI shooting function.
+
+        Args:
+            battlefield: Playable area. List of lists of square objects.
+
+        Returns:
+            position_x: X coordinate of the shoot made by AI.
+            position_y: Y coordinate of the shoot made by AI.
+
+        """
         position_x = randint(0, 9)
         position_y = randint(0, 9)
         target = battlefield.board[position_y][position_x]
@@ -26,7 +45,6 @@ class AI:
 
         else:
             self.unmarked.append(target)
-            # print(target)
             target.make_border()
 
         return (position_x, position_y)
