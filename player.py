9a3ca76020d0b5c3eb545c91ad05a3f766097899
Kacper Zelichowski <kafejek@gmail.com>
2017-06-16 09:38:15 +0200

    Player.py docstrings added.

--- player.py
+++ player.py
@@ -1,5 +1,3 @@
-# from ocean import Ocean
-# from ship import Ship
 from ai import AI
 import common
 
@@ -7,17 +5,33 @@ import common
 class Player:
 
     def __init__(self, name):
+        """Initialize function for the Player class.
+            Creates the player with name, is_alive, and previous_shot attributes.
 
+        Args:
+            name: Player name.
+
+        Returns:
+            none.
+
+        """
         self.name = name
         self.is_alive = True
         self.previous_shot = "Nothing"
 
     def shoot_on_board_player(self, battlefield, coords):
+        """Shot made by the player.
 
+        Args:
+            battlefield: Playable area.
+            coord: X, Y coordinates of the shot player wants to make.
+
+        Returns:
+            none
+
+        """
         pos_y, pos_x = common.convert_coords(coords)
         target = battlefield.board[pos_x][pos_y]
-        # print(pos_x, pos_y)
-        # print(target.__dict__)
 
         if target.is_ship:
             target.mark()
@@ -25,14 +39,30 @@ class Player:
             target.show()
 
     def shoot_on_board_ai(self, board):
+        """Shot made by the AI.
 
+        Args:
+            board: Playable area.
+
+        Returns:
+            target: Coords of the shot made by AI.
+
+        """
         ai = AI()
         target = ai.shoot_on_board_ai(board)
         self.previous_shot = target
         return target
 
     def check_if_alive(self, board):
+        """Checks if the player ships are sunk.
 
+        Args:
+            board: Playable area.
+
+        Returns:
+            none
+
+        """
         for ship in board.ships:
             if not ship.sunk:
                 break
@@ -40,4 +70,13 @@ class Player:
             self.is_alive = False
 
     def __str__(self):
+        """__str__ magic function. Used for username printing.
+
+        Args:
+            none
+
+        Returns:
+            Name attribute converted into a string.
+
+        """
         return str(self.name)
